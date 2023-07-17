7396063019ac3846e9e1654c9b6a9f4a46e515aa
Motty-har <harligmotty@gmail.com>
2023-07-17 19:22:34 -0400

    Created navbar

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBar(){
+    return(
+        <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="#books">Books</a>
+            <a href="#add-a-book">Add a Book</a>
+        </div>
+    )
+}
+
+export default NavBar
