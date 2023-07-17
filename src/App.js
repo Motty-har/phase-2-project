f4770b84bf2eb3a9e81d85848d117a23a203b697
Motty-har <harligmotty@gmail.com>
2023-07-17 19:25:09 -0400

    styled navbar and added to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React, {useEffect, useState} from 'react';
+import NavBar from "./NavBar"
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
     </div>
   );
 }
