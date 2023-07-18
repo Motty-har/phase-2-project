4a87b60484b04f8fa3f2b01915838955477f7698
Motty-har <harligmotty@gmail.com>
2023-07-18 10:56:27 -0400

    got first part of form functioning by updating state

--- src/BookForm.js
+++ src/BookForm.js
@@ -16,9 +16,9 @@ function BookForm({setBookForm}){
         <div>
             <form class="container">
                 <h2>Add a book!</h2>
-                <input placeholder="Title"></input>
-                <input placeholder="Image"></input>
-                <input placeholder="Author"></input>
+                <input placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}></input>
+                <input placeholder="Image" value={image} onChange={(e) => setImage(e.target.value)}></input>
+                <input placeholder="Author" value={author} onChange={(e) => setAuthor(e.target.value)}></input>
                 <label for="isRead">Have you read it?</label>
                 <select id="isRead">
                     <option value={true}>Yes</option>
