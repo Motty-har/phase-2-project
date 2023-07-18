26f79608088616ffa29b6a0241555fc39199af60
Motty-har <harligmotty@gmail.com>
2023-07-18 10:47:45 -0400

    set up book card wit correct information

--- src/BookCard.js
+++ src/BookCard.js
@@ -3,11 +3,10 @@ import React, {useState} from "react";
 function BookCard({book}){
     const {title, image, author, read} = book
     const [isRead, setIsRead] = useState(read)
-    
     return(
         <div class="card">
-            <h1 class="book-title">Title: </h1>
-            <img src={} class="book-img"/>
+            <h1 class="book-title">Title:   {title}</h1>
+            <img src={image} class="book-img"/>
             <p class="book-author">Author: {author}</p>
             <button class="button" onClick={() => setIsRead(!isRead)}>{isRead === false ? "Read Me" : "Read"}</button>
 
