5e1b211e1e307dec08681a6abb1b489a6271da93
Motty-har <harligmotty@gmail.com>
2023-07-17 20:32:28 -0400

    added bookcard component

---
+++ src/BookCard.js
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+
+function BookCard(){
+    const [isRead, setIsRead] = useState(read)
+    return(
+        <div class="card">
+            <h1 class="book-title">Title: </h1>
+            <img src={} class="book-img"/>
+            <p class="book-author">Author: {author}</p>
+            <button class="button" onClick={() => setIsRead(!isRead)}>{isRead === false ? "Read Me" : "Read"}</button>
+
+        </div>
+    )
+}
+
+export default BookCard
